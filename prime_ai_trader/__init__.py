d4ed47500398d66d33908d8a972d2f022748cdad
pegandoeladevolta-hub <pegandoeladevolta@gmail.com>
2026-08-26 23:25:44 -0300

    Atualiza branding e versão do Prime Trader

--- prime_ai_trader/__init__.py
+++ prime_ai_trader/__init__.py
@@ -1,3 +1,3 @@
-"""PRIME AI TRADER - assistente quantitativo desktop."""
+"""PRIME TRADER - terminal desktop com motor analítico baseado na v1.2.6."""
 
-__version__ = "1.2.6"
+__version__ = "1.4.0"
